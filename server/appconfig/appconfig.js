8c52167c9840bedb468df9108491622a811e2c25
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-10-26 11:58:59 +0530

    fixed few lint errors

--- server/appconfig/appconfig.js
+++ server/appconfig/appconfig.js
@@ -1,5 +1,9 @@
+/* eslint no-underscore-dangle: 0 */
 const path = require('path');
 const extend = require('util')._extend;
+const developmentConfig = require('./env/development');
+const testConfig = require('./env/test');
+const productionConfig = require('./env/production');
 
 const defaults = {
   SERVER_ROOT: path.resolve(__dirname, '../'),
@@ -7,9 +11,9 @@ const defaults = {
 };
 
 const appConfig = {
-  development: extend(require('./env/development.js'), defaults),
-  test: extend(require('./env/test.js'), defaults),
-  production: extend(require('./env/production.js'), defaults),
+  development: extend(developmentConfig, defaults),
+  test: extend(testConfig, defaults),
+  production: extend(productionConfig, defaults),
 };
 
 const effectiveENV = (process.env.NODE_ENV || 'development');
