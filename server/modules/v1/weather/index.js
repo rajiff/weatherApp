b7b79715c61751cd1e832397964e9d2551eacc30
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-11-02 11:01:24 +0530

    added webhook for dialog flow

--- server/modules/v1/weather/index.js
+++ server/modules/v1/weather/index.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
 const weatherAgent = require('./weatherAgent');
 
-router.get('/', (req, res) => {
+router.post('/', function (req, res) {
+  const city = (req.body.queryResult.parameters['geo-city'] || 'Bangalore');
+  let date = Date.now();
+  
+  // Get the date for the weather forecast (if present)
+  if (req.body.queryResult.parameters['date']) {
+    date = req.body.queryResult.parameters['date'];
+  }
+  
+  weatherAgent
+    .getWeather(city, date)
+    .then((result) => {
+      let response = {'fulfillmentText': result.result.message };
+      console.log("Returning response ", response)
+      res.json(response);
+    }, (err) => {
+      console.log("Error in weather request ", err);
+      res.json({ 'fulfillmentText': `I don't know the weather but I hope it's good!` });
+    });
+});
+
+router.get('/', function (req, res) {
   // res.send({ message: 'Got it thanks' });
 
   const city = (req.query.city || 'Bangalore');
